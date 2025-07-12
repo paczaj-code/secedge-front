adfe0d859642772d33df05f7dfe7067d1e1cd219
paczaj <marcinpaczkowski@icloud.com>
2025-07-12 02:16:33 +0200

    feat: add HeaderComponent, ThemeToggleComponent, and PrimeNG ButtonModule to AppComponent imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './ui/header/header.component';
+import { ButtonModule } from 'primeng/button';
+import { ThemeToggleComponent } from './ui/theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, ButtonModule, ThemeToggleComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })

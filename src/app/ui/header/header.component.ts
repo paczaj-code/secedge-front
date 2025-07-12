19c3b0059563d80ac962f7f303b322b71cefcad4
paczaj <marcinpaczkowski@icloud.com>
2025-07-12 02:23:55 +0200

    feat: integrate ThemeToggleComponent into HeaderComponent

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -2,10 +2,11 @@
  * Imports Component and OnInit from Angular core library
  */
 import { Component } from '@angular/core';
+import { ThemeToggleComponent } from '../theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [ThemeToggleComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
